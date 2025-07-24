3ebf89e1de236a9c2cb6623c9b13d3d30a8a6a40
Muhammad Ali <m.alibabar2004@gmail.com>
2025-07-24 04:21:18 -0700

    changing the url

--- client/src/axiosInstance.js
+++ client/src/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://server-cyan-one.vercel.app/api',
 });
 
 // Set Authorization token automatically from localStorage
