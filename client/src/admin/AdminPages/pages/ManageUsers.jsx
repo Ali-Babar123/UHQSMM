ba4890f4fb91587a1b7426f933f376853e469149
Muhammad Ali <m.alibabar2004@gmail.com>
2025-07-28 00:06:42 -0700

    update Manage users

--- client/src/admin/AdminPages/pages/ManageUsers.jsx
+++ client/src/admin/AdminPages/pages/ManageUsers.jsx
@@ -19,7 +19,7 @@ const ManageUsers = () => {
     const fetchUsers = async () => {
 
       try {
-        const res = await axiosInstance.get('https://server-cyan-one.vercel.app/api/auth/getUsers');
+        const res = await axiosInstance.get('/auth/getUsers');
         setUsers(res.data.users);
       } catch (err) {
         console.error('Error fetching users:', err.response?.data || err.message);
